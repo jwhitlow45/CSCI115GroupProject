ea37c1c75722df11436a9d7febc476fdb2388c3e
Ryan1Up <62736925+Ryan1Up@users.noreply.github.com>
2020-11-11 12:41:54 -0800

    Fixed bugs in RadixSort and CountSort
    Former-commit-id: e4f50f1ecc09941c0e1a84b56c885660f04b6824

--- radixSort.h
+++ radixSort.h
@@ -4,32 +4,25 @@
 
 int* radixSort(int* arr, int size, int i)
 {
-	int* C;
-	int* B;
-	//auxiliary array for counting
+	
 	try{
 	
-		B = new int[size+1];
-		C = new int[10]{0};
-	}catch(std::bad_alloc ba)
-	{
-		std::cout<<"Could not allocate sufficient memory"<<std::endl;
-		return arr;
-	}
-	
+		int* B = new int[size];
+		int* C = new int[10]{0};
 		
-		//init the count array to be full of 0's
-		C = new int[10]{0};
-		int index;
+		int index = 0;
 		
 		for(int j = 0; j < size; j++)
 		{
 	
+			B[j] = arr[j];	//init the "return" array as a copy of the original array
+			//This approach is used to be able to dynamically de-allocate the extra memory inside the function
+			
 			//count the number of times a certain value appears
 			//the integer is calculated by the number/(10^ radix) %10
 			//the reduces the integer down to a number, that when 
 			//you take the modulus 10 it returns a single digit value to be sorted by
-			index =(arr[j]/((int)(pow(10, i)))) % 10;
+			index =(arr[j]/((int)(pow(10, radix)))) % 10;
 			
 			C[index]++;
 		}
@@ -41,28 +34,44 @@ int* radixSort(int* arr, int size, int i)
 		for(int k = 1; k < 10; k++)
 		{	
 			// calculate the amount of elements that will come before the last of the current element
-			C[k] = C[k] + C[k-1];
+			C[k] += C[k-1];
 			
 		}
 			
-		//starting from the end, iterate backwards through the original array and place duplicates into the final array
+		//starting from the end, iterate backwards through the copy of the original array and place values into the original array corresponding
+		// to their relative index number saved in the auxiliary array C
 		for(int l = size-1; l >=0 ; l--)
 		{
+			//grab the "index" number at the current radix
+			index = (B[l]/(int)(pow(10, radix))) % 10;	
+			//
+			//put the value from the copied array into the original array by referencing the index number in C that corresponds
+			// to the calculated index above		
+			arr[C[index]] = B[l];
 			
-			index = (arr[l]/(int)(pow(10, i))) % 10;
-			
-			B[C[index]] = arr[l];
+			//decrement the relative index value in C to show that the element has already been placed
 			C[index]--;
 		}
-	
+		
+		// de-allocate the memory for the auxiliary arrays so your machine does not go boom
 		delete[] C;
-		return B;
+		delete[] B;
 			
+	}catch(std::bad_alloc ba)
+	{
+		std::cout<<"Could not allocate sufficient memory"<<std::endl;
+		
+	}catch(std::exception e)
+	{
+		std::cout<<"Array out of bounds probably"<<std::endl;
+	}
+	
 
 }
 
 void radixSort(int* arr, int size)
 {
+	
 	//Find the maximum element to determine the radix
 	int max = 0;
 	for(int i = 0; i < size; i++)
@@ -76,23 +85,12 @@ void radixSort(int* arr, int size)
 	{
 		radix++;
 		max /= 10;
-	}
-	int* temp = arr;
-	std::cout<<radix<<std::endl;
+	}	
+
+	// repeat the counting sort method for each radix value
 	for(int i = 0; i < radix; i++)
 	{
-		int* delTemp = temp;
-		temp = radixSort(temp, size, i);
-		if(delTemp != temp)
-		{
-			delete[] delTemp;
-		} 		
+		radixSort(arr, size, i);
 	}
-	arr = new int[size];
-	for(int q = 0; q < size; q++)
-	{
-		arr[q] = temp[q];
-	}
-
 
 }
