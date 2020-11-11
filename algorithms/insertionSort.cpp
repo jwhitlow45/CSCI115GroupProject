23cbd4abf265ccb460f259cf0917e076e5bfef54
jwhitlow45 <jwhitlow45@gmail.com>
2020-11-11 11:15:02 -0800

    Commented insertion sort

--- algorithms/insertionSort.cpp
+++ algorithms/insertionSort.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 void insertionSort(int arr[], size_t size)
 {
-	int i = 1;
-	while (i < size)
+	int i = 1;			//set i to second element in array (first element is sorted in while loop)
+	while (i < size)	//iterate through entire array
 	{
-		int j = i;
-		while (j > 0 && arr[j - 1] > arr[j])
-		{
-			swap(arr[j], arr[j - 1]);
-			j--;
+		int j = i;		//set j to i
+		while (j > 0 && arr[j - 1] > arr[j])	//while j is in the array
+		{										//and prev element > current element
+			swap(arr[j], arr[j - 1]);				//swap prev and current element
+			j--;									//dec j
 		}
-		i++;
+		i++;									//inc i
 	}
 }
