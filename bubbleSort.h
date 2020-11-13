d5570155609b070ae28b1ed12fd0ee10e1b11a7e
RichardPhist <74016850+RichardPhist@users.noreply.github.com>
2020-11-12 18:08:07 -0800

    Update bubbleSort.h

--- bubbleSort.h
+++ bubbleSort.h
@@ -1,9 +1,9 @@
 #pragma once
 
 void bubbleSort(int* a, int n) { //bubbleSort using arrays
-	for (int i = 1; i < n; i++) { //loop starts at the beginning
-		for (int j = n - 1; j > i; j--) { //loop starts at the end
-			if (a[j] < a[j - 1]) { //compares j and j-1 with eachother
+	for (int i = 1; i < n; i++) { //i loop starts at the beginning and iterates until the end of the array
+		for (int j = n - 1; j > i; j--) { //j loop starts at the end of the array and decrements till i+1
+			if (a[j] < a[j - 1]) { //compares j and j-1 with each other
 				int temp = a[j]; //swaps j and j-1 if j-1 is larger than j
 				a[j] = a[j - 1];
 				a[j - 1] = temp;
