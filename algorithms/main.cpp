4e82663a3f58969e3c428746c1b5cfe531bbc5dd
jwhitlow45 <jwhitlow45@gmail.com>
2020-11-22 13:17:15 -0800

    Built main to be interactive

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -1,4 +1,8 @@
-#include<iostream>
+#include<iostream>	//output to console
+#include <string>	//user input
+#include <ctime>	//measure algorithm times
+#include<fstream>	//outputing results to txt file
+
 #include "bubbleSort.h"
 #include "countingSort.h"
 #include "insertionSort.h"
@@ -7,19 +11,67 @@
 #include "radixSort.h"
 #include "selectionSort.h"
 #include "heapSort.h"
-#include <string>	// input verification
-#include<cstdlib>	//srand and rand
-#include <ctime>	// reseting seed for quickSort
-#include <vector> // output times
-#include<fstream>
+
 using namespace std;
 
-static const int GLOBAL_MAX = 200000000;
-std::vector<string> sortNames = {"Insertion Sort", "Selection Sort",
-								"Bubble Sort", "Merge Sort", "Quick Sort",
-								"Heap Sort", "Counting Sort", "Radix Sort"};
+//returns sorted array of size size
+int* getSortArr(int size)
+{
+	int* arr = new int[size];
+	for (size_t i = 0; i < size; i++)
+		arr[i] = i;
+	return arr;
+}
 
-//Helper function to call the chosen sorting algorithm
+//returns reverse sorted array of size size
+int* getReverseSortArr(int size)
+{
+	int* arr = new int[size];
+	for (size_t i = 0; i < size; i++)
+		arr[i] = size - i - 1;
+	return arr;
+}
+
+//returns random array of size size, range [0-50,000)
+int* getRandomArr(int size)
+{
+	srand(4201337);
+	int* arr = new int[size];
+	for (size_t i = 0; i < size; i++)
+		arr[i] = rand() % 50000;
+	return arr;
+}
+
+//returns array of size size and type arrType
+/*
+	0-sorted
+	1-reverse sorted
+	2-random
+*/
+int* getArr(int size, int arrType)
+{
+	switch (arrType)
+	{
+	case 0:
+		return getSortArr(size);
+	case 1:
+		return getReverseSortArr(size);
+	case 2:
+		return getRandomArr(size);
+	}
+}
+
+//Funciton allowing calling of specific algorithms
+/*
+	0-insertionSort
+	1-selectionSort
+	2-bubbleSort
+	3-mergeSort
+	4-quickSort
+	5-heapSort
+	6-countingSort
+	7-radixSort
+*/
 void sort(int* arr, int size, int algNum)
 {
 	switch (algNum)
@@ -48,83 +100,78 @@ void sort(int* arr, int size, int algNum)
 	case 7:
 		radixSort(arr, size);
 		break;
-	}
-}
-
-int* getSortArr(int size)
-{
-	int* arr = new int[size];
-	for (size_t i = 0; i < size; i++)
-		arr[i] = i;
-	return arr;
-}
-
-int* getReverseSortArr(int size)
-{
-	int* arr = new int[size];
-	for (size_t i = 0; i < size; i++)
-		arr[i] = size - i - 1;
-	return arr;
-}
-
-int* getRandomArr(int size)
-{
-	srand(4201337);
-	int* arr = new int[size];
-	for (size_t i = 0; i < size; i++)
-		arr[i] = rand() % 50000;
-	return arr;
-}
-
-int* getArr(int size, int arrType)
-{
-	switch (arrType)
-	{
-	case 0:
-		return getSortArr(size);
-	case 1:
-		return getReverseSortArr(size);
-	case 2:
-		return getRandomArr(size);
+	default:
+		cout << "ERROR: Not a valid input" << endl;
 	}
 }
 
 int main()
 {
-	int arrSizes[5] = { 1000000,5000000,10000000,100000000,200000000 };
+	string userSortAlgo;	//sorting algo from user
+	string userArrSize;		//arr size from user
+	string userArrOrder;	//arr order from user
+	string userNumRepeat;	//number of times to repeat from user
+	char userResponse;		//reponse from user to run algorithm again
+
+	//indexed algorithm names and arr orders to allow output to be organized
+	string sortNames[8] = { "Insertion Sort","Selection Sort",
+							"Bubble Sort", "Merge Sort", "Quick Sort",
+							"Heap Sort","Counting Sort", "Radix Sort" };
 	string arrOrder[3] = { "inOrder","reverseOrder","randomOrder" };
 	fstream FILE;
-	FILE.open("output-n.txt", ios::out);
-	for (size_t sortAlgo = 5; sortAlgo < 8; sortAlgo++)
-	{//different sorting algorithms
-		for (size_t arrSize = 0; arrSize < 5; arrSize++)
-		{//different array sizes
-			for (size_t order = 0; order < 3; order++)
-			{//different array orders
-				for (size_t i = 0; i < 5; i++)
-				{//run 5 times each
-					//created new array of proper size
-					int* arr = getArr(arrSizes[arrSize], order);
+	FILE.open("output.txt", ios::out);
 
-					clock_t time_t = clock();	//start time
+	do
+	{
+		cout << "0 - insertionSort\n"
+				<< "1 - selectionSort\n"
+				<< "2 - bubbleSort\n"
+				<< "3 - mergeSort\n"
+				<< "4 - quickSort\n"
+				<< "5 - heapSort\n"
+				<< "6 - countingSort\n"
+				<< "7 - radixSort\n";
+		cout << "Algorithm to run?: ";
+		getline(cin, userSortAlgo);
+		cout << "\nNOTE:Algorithms can stack overflow due to space complexity";
+		cout << "\nSize of array?: ";
+		getline(cin, userArrSize);
+		cout << "\n0 - sorted\n"
+				<< "1 - reverse sorted\n"
+				<< "2 - random\n";
+		cout << "Array order?: ";
+		getline(cin, userArrOrder);
+		cout << "\nNumber of times to repeat?: ";
+		getline(cin, userNumRepeat);
+		cout << endl << endl;
 
-					sort(arr, arrSizes[arrSize], sortAlgo); //perform sort
+		//run test user specified number of times
+		for (size_t i = 0; i < stoi(userNumRepeat); i++)
+		{
+			//created new array of user specified size and order
+			int* arr = getArr(stoi(userArrSize), stoi(userArrOrder));
 
-					time_t = clock() - time_t;	//end time
-					string output = sortNames[sortAlgo] + "\nSize:" +
-						to_string(arrSizes[arrSize])+ "\nOrder:" +
-						arrOrder[order]+ "\nTime:" +
-						to_string(double(time_t) / CLOCKS_PER_SEC)+
-						" seconds\n\n";
-					cout << output;
-					FILE << output;
-					delete arr;
-				}
-			}
+			clock_t time_t = clock();	//start time
+
+			sort(arr, stoi(userArrSize), stoi(userSortAlgo)); //perform sort
+
+			time_t = clock() - time_t;	//end time
+			string output = sortNames[stoi(userSortAlgo)] +
+				"\nSize:" + userArrSize +
+				"\nOrder:" + arrOrder[stoi(userArrOrder)] + 
+				"\nTime:" + to_string(double(time_t) / CLOCKS_PER_SEC) +
+				" seconds\n\n";
+			cout << output;	//print results to console
+			FILE << output;	//store results in file
+			delete[] arr;
 		}
-		cout << endl;
-	}
 
-	cout << "DONE.";
+		cout << "Run another algorithm?(y/n): ";
+		cin >> userResponse;
+		cin.ignore();
+
+	} while (tolower(userResponse) == 'y');
+
+	FILE.close();
 	return 0;
 }
